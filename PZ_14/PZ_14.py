b57a9f03ef02a1dc115166b9dc2f65b79d028581
Kamehameha127 <sogenovr963@gmail.com>
2025-04-11 14:25:48 +0300

    commit

--- PZ_14/PZ_14.py
+++ PZ_14/PZ_14.py
@@ -1,8 +1,12 @@
 # Вариант 31
 # Из исходного текстового файла (price.txt) выбрать все цены. Посчитать количество полученных элементов.
 
-file = open("price.txt", 'r')
+file = open("price.txt", 'r', encoding='utf-8').read()
 
 prices = 0
 
-print(file.read().split())
+for cop in file.split():
+     if 'коп.' in cop.replace(',', ''):
+          prices += 1
+
+print(prices)
